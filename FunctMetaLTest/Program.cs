26bcdc2beb56500136aeda7e22fb1b0ced15d97b
unknown <ayayalar@.(none)>
2009-12-06 21:26:35 -0800

    initial checkin for FunctMetaLTest

---
+++ FunctMetaLTest/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FunctMetaL.Core;
+
+namespace FunctMetaLTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FunctMetaL.Core.Main main = new Main();
+            main.TestSuite = @"C:\Projects\FunctMetaL\Xml\AcceptanceTest.xml";
+            main.Start();
+            Console.ReadLine();
+        }
+    }
+}
